f8c7f1eea4f3640ee96bbf5e016d1d9000262bf6
Dollars <xbarthel@gmail.com>
2014-12-24 11:59:37 +0100

    [Enh] floor creation addon

--- src/floor_repartition.py
+++ src/floor_repartition.py
@@ -1,20 +1,49 @@
 import bpy
+from bpy.props import *
 import numpy as np
 
-def create_plane(width, height):
-	bpy.ops.mesh.primitive_plane_add()
-	floor = bpy.data.objects['Plane']
+bl_info = {"name": "Easy City Addon", "category": "Object"}
+
+def add_floor(context, width, height):
+	bpy.ops.mesh.primitive_grid_add(x_subdivisions=width*2, y_subdivisions=height*2, enter_editmode=True)
+	floor = bpy.data.objects['Grid']
 	floor.name = "Floor"
 	bpy.context.scene.objects.active = floor
 
 	floor.scale.x = width
 	floor.scale.y = height
 
-	bpy.ops.object.mode_set(mode='EDIT')
-	bpy.ops.mesh.subdivide(number_cuts=height)
-
 	return floor
 
+class MESH_OT_primitive_floor_add(bpy.types.Operator):
+    '''Add a floor'''
+    bl_idname = "mesh.primitive_floor_add"
+    bl_label = "Add floor"
+    bl_options = {'REGISTER', 'UNDO'}
+ 
+    width = IntProperty(name="Width",
+            description="Width of the plane",
+            default=5, min=2, max=500)
+    height = IntProperty(name="Height",
+            description="Height of the plane",
+            default=5, min=2, max=500)
+ 
+    def execute(self, context):
+        ob = add_floor(context, self.width, self.height)
+        return {'FINISHED'}
+ 
+def menu_func(self, context):
+    self.layout.operator("mesh.primitive_floor_add", 
+        text="Floor", 
+        icon='MESH_GRID')
+ 
+def register():
+   bpy.utils.register_module(__name__)
+   bpy.types.INFO_MT_mesh_add.prepend(menu_func)
+ 
+def unregister():
+    bpy.utils.unregister_module(__name__)
+    bpy.types.INFO_MT_mesh_add.remove(menu_func)
+ 
 if __name__ == "__main__":
-
-	create_plane(10,20)
+    register()
